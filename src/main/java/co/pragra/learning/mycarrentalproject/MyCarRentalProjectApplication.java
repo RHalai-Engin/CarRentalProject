eeb6fc0056d7c8baea2d574a4dc0681f106434a6
Rajan Halai <rajkumarhalai0@gmail.com>
2024-04-27 12:52:01 -0400

    Car Rental Project initiated

---
+++ src/main/java/co/pragra/learning/mycarrentalproject/MyCarRentalProjectApplication.java
@@ -0,0 +1,13 @@
+package co.pragra.learning.mycarrentalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCarRentalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCarRentalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/pragra/learning/mycarrentalproject/MyCarRentalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.pragra.learning.mycarrentalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCarRentalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
